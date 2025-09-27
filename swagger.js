466af04736d27bf9054bc15dfdb7916059f7fb4b
WACD <walejandrocamacho@gmail.com>
2025-09-27 11:39:23 -0600

    Update

--- swagger.js
+++ swagger.js
@@ -1,12 +1,14 @@
 const swaggerAutogen = require('swagger-autogen')();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const doc = {
   info: {
     title: 'My Contacts API',
     description: 'Contacts API for CSE341'
   },
-  host: 'localhost:3000',
-  schemes: ['http']
+  host: isProduction ? 'cse341-p.onrender.com' : 'localhost:3000',
+  schemes: [isProduction ? 'https' : 'http']
 };
 
 const outputFile = './swagger.json';
